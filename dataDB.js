1cf9bbee5bde7aca8d7c6409ea0933f6c9b02369
burhanfauzisalam <burhanfauzisalam@gmail.com>
2025-01-21 13:31:15 +0700

    AligndAnalyze

---
+++ dataDB.js
@@ -0,0 +1,22 @@
+const { queryDatabase } = require("./db");
+
+const createDataTable = async() => {
+  const sql = `
+    CREATE TABLE IF NOT EXISTS data (
+      id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      customID varchar(50) DEFAULT NULL,
+      data varchar(50) DEFAULT NULL,
+      time datetime DEFAULT CURRENT_TIMESTAMP
+    )
+  `;
+
+  try {
+    // Menjalankan query untuk membuat tabel
+    await queryDatabase(sql);
+    console.log("Table 'data' created successfully or already exists.");
+  } catch (err) {
+    console.error("Error creating table: ", err);
+  }
+};
+
+module.exports = createDataTable;
